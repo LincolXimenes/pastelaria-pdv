89d8e0cf3180992743a5434c17bba10cd78fe435
Tfonseca200 <thiagofonsecaclaudino@gmail.com>
2025-08-10 23:09:43 -0300

    test: adicionando testes unitarios pro controller criarCliente

--- jest.config.js
+++ jest.config.js
@@ -156,7 +156,10 @@ const config = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-   testMatch: ["**/**/*.spec.js"],
+  // testMatch: [
+  //   "**/__tests__/**/*.?([mc])[jt]s?(x)",
+  //   "**/?(*.)+(spec|test).?([mc])[jt]s?(x)"
+  // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
